cf182229633a7c2b6045f96c4de240478585923d
Yu357 <forestgreen14689@gmail.com>
2023-05-01 15:58:31 +0900

    feat: 画像を表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,9 @@ export default function Home() {
 			</Head>
 
 			<main>
-				
+
 				<TextSection
+					image="/images/headers/development.png"
 					large
 					title="つくってみたいものをつくる"
 					detail={`専門学校に入学してからそろそろ3年。自分が作ってみたいものを自由に作って暮らしていたら、いつの間にかたくさんの作品ができていました。\nせっかくなので、今まで趣味で作ってきた作品をまとめてみました。ぜひご覧ください!`}
