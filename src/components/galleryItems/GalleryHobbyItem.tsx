1f255cbe18ea7e141976587a2b43fef902f502ea
Yu357 <forestgreen14689@gmail.com>
2023-05-05 07:52:58 +0900

    feat: GalleryHobbyItemのimgをImageに置き換え

--- src/components/galleryItems/GalleryHobbyItem.tsx
+++ src/components/galleryItems/GalleryHobbyItem.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 interface Props {
 	image: string
 	title?: string
@@ -8,7 +10,13 @@ function GalleryHobbyItem(props: Props) {
 	return (
 		<div>
 
-			<img src={props.image} alt={props.title ?? "Hobby"} className="border" />
+			<Image
+				src={props.image}
+				alt="--"
+				width={900}
+				height={600}
+				className="border"
+			/>
 
 			<div className="mt-4 text-center ">
 				<span className="text-gray-600">{props.title ?? ""}</span>
