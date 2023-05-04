b57529b91cd0489fbd4809bc3a8ec85830ff83d4
Yu357 <forestgreen14689@gmail.com>
2023-05-04 09:56:02 +0900

    feat: 404ページを実装

---
+++ src/pages/404.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import Footer from "../components/sections/Footer"
+import Header from "../components/sections/Header"
+import TextSection from "../components/sections/TextSection"
+import Head from "next/head"
+
+function NotFoundPage() {
+
+	return (
+		<>
+
+			<Head>
+				<title>Not found - Forestic</title>
+			</Head>
+
+			<main>
+				<TextSection
+					title="Page Not Found"
+					detail="ページが見つかりませんでした。"
+					detailCenter
+					noDivider
+					className="mt-6"
+				/>
+
+				<div className="mt-20 text-center">
+					<Link href="/" className="black-button">トップに戻る</Link>
+				</div>
+			</main>
+		</>
+	)
+}
+
+export default NotFoundPage
